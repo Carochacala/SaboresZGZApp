8a0445983e42fee071262dfbc385cfcf8af9feeb
JESSIE1710 <jessicaxhxc17@gmail.com>
2026-04-07 00:10:04 +0200

    RecyclerView terminado y Manifest restaurado por la prueba

--- app/src/main/java/com/comenendez/saboreszgz/SeleccionRestaurantes.java
+++ app/src/main/java/com/comenendez/saboreszgz/SeleccionRestaurantes.java
@@ -7,18 +7,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class SeleccionRestaurantes extends AppCompatActivity {
+    /* clase donde se selecciono el pais y muestra sus restaurantes
+
+     */
+    RecyclerView listaVisualRestaurantes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_seleccion_restaurantes);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //  Conectaremos la variable de Java con el ID del XML
+        listaVisualRestaurantes=findViewById(R.id.rvRestaurantes);
+        //Le decimos a la lista que ordene las tarjetas de arriba hacia abajo,de preferencia en este caso linearlayout
+        listaVisualRestaurantes.setLayoutManager(new androidx.recyclerview.widget.LinearLayoutManager(this));
+
+        //EJEMPLO PRUEBAS MOMENTANEO
+        // 1. Preparamos los datos de prueba
+        List<ModeloCajitaRestaurante> listaDePrueba = new java.util.ArrayList<>();
+
+        listaDePrueba.add(new ModeloCajitaRestaurante(
+                "La Parrilla Vegana",
+                "Comida deliciosa sin origen animal",
+                4.5f,
+                1.2f,
+                R.drawable.fondo_cuadrado,
+                ""
+        ));
+
+        listaDePrueba.add(new ModeloCajitaRestaurante(
+                "El Rincón del Café",
+                "Desayunos y meriendas especiales",
+                5.0f,
+                3.5f,
+                R.drawable.fondo_cielo,
+                ""
+        ));
+        //le entrego al adaptador mi lista de prueba
+       RestauranteAdapter miAdaptadorRestaurante= new RestauranteAdapter(listaDePrueba);
+       //y le comunicamos a la lista que ese sera su adaptador
+        listaVisualRestaurantes.setAdapter(miAdaptadorRestaurante);
+
+
+
     }
 }
