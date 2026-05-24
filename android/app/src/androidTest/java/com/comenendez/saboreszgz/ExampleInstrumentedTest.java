da52b009b2069be28120121737508d7c92b98a9a
Carol <carochacala@gmail.com>
2026-05-24 13:44:05 +0200

    Finalizando 24_05 13:43

--- android/app/src/androidTest/java/com/comenendez/saboreszgz/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/comenendez/saboreszgz/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.comenendez.sabores_zgz", appContext.getPackageName());
+        assertEquals("com.comenendez.saboreszgz", appContext.getPackageName());
     }
 }
